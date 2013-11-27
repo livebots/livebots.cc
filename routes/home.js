dd2ec32e92816303cb372aa749f50cf65f835bad
David Dias <daviddias.p@gmail.com>
2013-11-27 06:12:48 +0000

    migrating to a more hapi way

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,5 @@
 var server        = require('./../index.js');
-var homeHandler          = require('./../controllers/home.js');
-
+var homeHandler   = require('./../controllers/home.js');
 
 var home = {
   method: 'GET',
